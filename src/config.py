340c30fc73737566b1ba1c7a38449da41f9cee24
harvdennis <harvey@dennisonline.co.uk>
2025-03-27 17:37:08 +0000

    chore: nice and clean

--- src/config.py
+++ src/config.py
@@ -4,8 +4,10 @@ class BaseConfig:
     DATA_DIR = Path(__file__).parent.parent / "data"
     TRAIN_FILE = DATA_DIR / "train.csv"
     DEV_FILE = DATA_DIR / "dev.csv"
-    AUG_TRAIN_FILE = DATA_DIR / "aug_train.csv"
+    AUG_TRAIN_FILE = DATA_DIR / "train_augmented.csv"
+    SAVE_DIR = DATA_DIR / "results"
 
+    
 def get_config() -> BaseConfig:
     return BaseConfig()
 
